e6e4a1462f878cd4802b85ef69dbeb6a57babd09
Joshua <joshuaray@OlloMBP.local>
2012-06-01 08:37:29 -0500

    intro statement on homepage

--- wp-content/themes/ollo/home.php
+++ wp-content/themes/ollo/home.php
@@ -3,21 +3,37 @@
 Template Name: Home
 */
 
-    get_header();
-?>
-
-
-
-
-
-
-
-
-
-
-
-<?php
-
-get_footer();
+get_header();
 
 ?>
+
+<div id="content" class="container-fluid">
+    <div id="intro">
+            <h1 class="introMsg">I love open source technologies, <br />
+                Hate comic Sans and love making the web <br /> 
+                a bit more awesome day by day. </h1>
+    </div>
+    <div id="homeWork">
+        
+    </div>
+    
+    <div id="homeWords">
+        
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
