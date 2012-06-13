8755d782bc37029ed328ef2dffc2710dbbc8e844
Joshua <joshuaray@OlloMBP.local>
2012-06-13 12:45:26 -0500

    removed comment threaded comments and moved enqueue to footer

--- wp-content/themes/ollo/header.php
+++ wp-content/themes/ollo/header.php
@@ -19,10 +19,9 @@
 
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
-	if ( is_singular() && get_option( 'thread_comments' ) )
-	wp_enqueue_script( 'comment-reply' );
 
 	wp_head();
+
 ?>
 </head>
 <body <?php body_class(); ?>>
