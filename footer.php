2de551a23dffadea4f0cc9ff376c9f3508d3a85a
Ollo <joshua@ollomedia.com>
2012-08-05 14:35:22 -0500

    footer goodness, and homepage cleanup

--- footer.php
+++ footer.php
@@ -13,13 +13,17 @@
     <div class="push"></div>
     <section id="footer">        
         <div id="footerText" class="inner clearfix">
-		    <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> is Proudly Powered by <a href="http://wordpress.org" title="the semantic publishing platform">WordPress</a> and <a href="" title="">Twitter Bootstrap</a>
+            <div class="credits left heading">
+		        <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> 
+		        is Proudly Powered by <a href="http://wordpress.org" title="the semantic publishing platform">WordPress</a> 
+		        and <a href="" title="">Twitter Bootstrap</a>
+		    </div>
 		    
 		    <ul id="social" class="right">
-		        <li><a href="#" class="twitter" title="Twitter"></a></li>
-            	<li><a href="#" class="forrst" title="Forrst"></a></li>
-            	<li><a href="#" class="linkedin" title="LinkedIn"></a></li>
-            	<li><a href="#" class="feed" title="RSS Feed"></a></li>		        
+		        <li><a href="http://twitter.com/pdxollo" target="_blank" class="twitter btnHover" title="Twitter Feed for updates"></a></li>
+            	<li><a href="https://forrst.com/people/Ollo" class="forrst btnHover" target="_blank" title="Forrst Development and Design Community"></a></li>
+            	<li><a href="http://www.linkedin.com/in/pdxollo" class="linkedin btnHover" target="_blank" title="LinkedIn Professional Profile"></a></li>
+            	<li><a href="https://github.com/ollo" class="github btnHover" target="_blank" title="Github Projects"></a></li>		        
 		    </ul>
 		</div>
     </section>
--- home.php
+++ home.php
@@ -2,97 +2,46 @@
 /*
 Template Name: Home
 */
-
 get_header();
-
 ?>
-
     <section id="intro" class="row-fluid">
         <div id="introContent" class="inner">
             <?php
-                    // The Loop
-                    while ( have_posts() ) : the_post();
-                    	the_content();
-                    endwhile;
-
-                // Reset Query
-                wp_reset_query();
+                while ( have_posts() ) : the_post();
+                	the_content();
+                endwhile; wp_reset_query(); 
             ?>
         </div>
     </section>
-    <section id="homeWork" class="row-fluid">
-        <div id="workInner" class="inner clearfix">
-            <div id="workMsg" class="column left">
-                <?php 
-                    $args = array(
-                        'pagename' => 'work'
-                    );
-
-                    // the query 
-                    $workPage_query = new WP_Query( $args );
-
-                    // The Loop
-                    while ( $workPage_query->have_posts() ) : $workPage_query->the_post();
-                    ?>
-                    <h1 class="heading"><a href="<?php the_permalink(); ?>">RECENT WORK</a></h1>
-                    <p><?php print_excerpt(400); ?></p>
+    <section>
+        <div id="homeWork" class="row-fluid">
+            <div id="workInner" class="inner clearfix">
+                <div id="workMsg" class="column left">
+                    <?php 
+                        $args = array( 'pagename' => 'work');
                     
-                    <?php    
-                    endwhile;
-
-                    // Reset Post Data
-                    wp_reset_postdata();
-
-                  ?>
+                        $workPage_query = new WP_Query( $args );
+                        while ( $workPage_query->have_posts() ) : $workPage_query->the_post(); ?>
+                            <h1 class="heading shadowTextDark"><a href="<?php the_permalink(); ?>">RECENT WORK</a></h1>
+                            <p><?php print_excerpt(400); ?></p>
+                        
+                    <?php endwhile;  wp_reset_postdata(); ?>
+                </div>
             </div>
-            
-            <div id="workSlider" class="carousel slide  column right">
-            <?php 
-                $args = array(
-                    'post_count' => 5,
-                    'post_type' => 'portfolio'
-                );
-    
-                // the query 
-                $work_query = new WP_Query( $args );
-
-                // The Loop
-                while ( $work_query->have_posts() ) : $work_query->the_post();
-                ?>                        
-                      <!-- Carousel items -->
-                      <div class="carousel-inner">
-                        <div class="item ">
-                            <?php the_post_thumbnail('medium'); ?>
-                            <div class="carousel-caption">
-                                <h4><?php the_title(); ?></h4>
-                                <p><?php the_excerpt(); ?></p>
-                            </div>
-                        </div>
-                      </div>
-                      <!-- Carousel nav -->
-                      <a class="carousel-control left" href="#workSlider" data-slide="prev">&lsaquo;</a>
-                      <a class="carousel-control right" href="#workSlider" data-slide="next">&rsaquo;</a>            
-                <?php    
-                endwhile;
-
-                // Reset Post Data
-                wp_reset_postdata();
-
-              ?>
-              </div>  
         </div>
     </section>
     
-    <section id="homeWords" class="row-fluid">
-        <div class="inner">
-            <div id="wordMsg" class="column right">
-                <h1 class="heading">RECENT WORDS</h1>
-                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            </div>
-            <div id="workFeatured" class="column right">
+    <section>
+        <div id="homeWords" class="row-fluid">
+            <div class="inner">
+                <div id="wordMsg" class="column right">
+                    <h1 class="heading shadowTextLight">RECENT WORDS</h1>
+                    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                </div>
+                <div id="workFeatured" class="column right">
                 
+                </div>
             </div>
         </div>        
     </section>
-
 <?php get_footer(); ?>
